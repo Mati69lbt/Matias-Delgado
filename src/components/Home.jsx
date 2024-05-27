eaee32f455eedd423864bed176a9ccb2a9493c6a
Mati69lbt <mati69_lbt@hotmail.com>
2024-05-27 10:18:42 -0300

    aa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@
 
 const Home = () => {
   return (
-    <div className="text-center md:p-32  py-20">
+    <div className="text-center md:p-32 py-20 w-full">
       <h1 className="md:text-6xl font-bold mb-2 text-5xl">Matias Daniel Delgado</h1>
       <h3 className="text-4xl font-medium text-gray-600">
         Full Stack Developer
