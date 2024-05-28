b4270e501d5929ae67f62c46d3dc29c3a869794a
Mati69lbt <mati69_lbt@hotmail.com>
2024-05-28 16:44:56 -0300

    servicios

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,5 +1,40 @@
 const Servicios = () => {
-  return <h1 className="font-bold text-5xl p-10">Servicios</h1>;
+  return (
+    <div className="p-10">
+      <h1 className="font-bold text-5xl mb-6">Servicios</h1>
+      <div className="m-3 sm:m-12 flex flex-col">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2 divide-y sm:divide-none list-disc pl-6 shadow-xl rounded-2xl">
+          <li>Desarrollo Web Completo</li>
+          <li>Diseño de Interfaces de Usuario (UI)</li>
+          <li>Desarrollo de Aplicaciones Móviles</li>
+          <li>Gestión de Bases de Datos</li>
+          <li>Optimización de Rendimiento</li>
+          <li>Integración de APIs</li>
+          <li>Automatización de Procesos</li>
+          <li>Control de Versiones</li>
+          <li>Despliegue en la Nube</li>
+          <li>Desarrollo de CMS</li>
+          <li>Soporte Técnico</li>
+          <li>Análisis de Datos</li>
+          <li>Desarrollo de Aplicaciones Empresariales</li>
+          <li>Consultoría Tecnológica</li>
+          <li>Gestión de Proyectos</li>
+          <li>Búsqueda y Análisis de Información</li>
+          <li>Documentación Técnica</li>
+          <li>Creación de Presentaciones</li>
+          <li>Gestión de Correo Electrónico y Calendarios</li>
+          <li>Creación y Edición de Documentos</li>
+          <li>Optimización de Procesos en Office</li>
+          <li>Análisis y Visualización de Datos en Excel</li>
+          <li>Manejo de Paquete de Office</li>
+          <li>Administración de Sistemas</li>
+          <li>Desarrollo y Mantenimiento de Intranets</li>
+          <li>Diseño Gráfico Básico</li>
+          <li>Formación y Capacitación</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Servicios;
